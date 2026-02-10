b5f3793e5ecfa784303e628872833972b9c5e687
Laura Won <139052598+ProjectHopper@users.noreply.github.com>
2026-02-10 12:21:58 -0500

    2.1_Data Layer Implementation_BibleModel.swift

---
+++ BibleModel.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct Verse: Codable, Identifiable {
+    var id: String { date }
+    let date: String
+    let content_kr: String
+    let content_en: String
+    let reference: String
+}
